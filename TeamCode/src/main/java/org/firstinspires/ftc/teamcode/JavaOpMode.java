d4c45c444cb9c4d2d876e49001b25ef5decc8fe9
Jade Turner <spacey-sooty@proton.me>
2024-11-03 17:36:13 +0800

    Added armMotor, worked on Auto
    Author: Sean Chan <sean.chan3@student.education.wa.edu.au>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaOpMode.java
@@ -13,6 +13,9 @@ public class JavaOpMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftMotor;
     private DcMotor rightMotor;
+    private DcMotor armMotor;
+
+//    private DcMotor intakeMotor;
 
     @Override
     public void runOpMode() {
@@ -21,9 +24,12 @@ public class JavaOpMode extends LinearOpMode {
 
         leftMotor = hardwareMap.get(DcMotor.class, "motor2");
         rightMotor = hardwareMap.get(DcMotor.class, "motor3");
+        armMotor = hardwareMap.get(DcMotor.class, "motor1");
+//        intakeMotor = hardwareMap.get(DcMotor.class, "motor0");
 
         leftMotor.setDirection(DcMotor.Direction.FORWARD);
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
@@ -31,14 +37,18 @@ public class JavaOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             double leftPower;
             double rightPower;
+            double armPower;
 
             double drive = -gamepad1.left_stick_y;
             double turn = -gamepad1.right_stick_x;
+            double armTurn = -gamepad2.left_stick_y;
             leftPower = Range.clip(drive + turn, -1.0, 1.0);
             rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            armPower = Range.clip(armTurn, -1.0, 1.0);
 
             leftMotor.setPower(leftPower);
             rightMotor.setPower(rightPower);
+            armMotor.setPower(armPower);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDrive.java
@@ -8,32 +8,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
 public class RobotAutoDrive extends LinearOpMode {
-
-    /* Declare OpMode members. */
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
 
     private ElapsedTime runtime = new ElapsedTime();
     static final double COUNTS_PER_MOTOR_REV = 1440;
     static final double DRIVE_GEAR_REDUCTION = 1.0;
     static final double WHEEL_DIAMETER_INCHES = 4.0;
-    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
 
     @Override
     public void runOpMode() {
-
-        // Initialize the drive system variables.
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -42,16 +34,12 @@ public class RobotAutoDrive extends LinearOpMode {
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at",  "%7d :%7d",
-                leftDrive.getCurrentPosition(),
-                rightDrive.getCurrentPosition());
+        telemetry.addData("Starting at", "%7d :%7d", leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
         telemetry.update();
 
         // Wait for the game to start (driver presses START)
         waitForStart();
-
-        // Step through each leg of the path,
-        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        
         encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
